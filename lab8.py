1e65fb23c2efc88ef781a5aef563ed6364391be4
computerscience22 <sciencecomputer611@gmail.com>
2025-06-15 21:12:26 +0530

    Create lab8.py

---
+++ lab8.py
@@ -0,0 +1,16 @@
+from tensorflow.keras.datasets import mnist
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load the MNIST dataset
+(X_train, y_train), (_, _) = mnist.load_data()
+
+# Print 4 images in a row
+plt.figure(figsize=(10, 5))
+for i in range(4):
+    plt.subplot(1, 4, i+1)
+    plt.imshow(X_train[i], cmap='gray')
+    plt.title(f"Label: {y_train[i]}")
+    plt.axis('off')
+plt.tight_layout()
+plt.show()
